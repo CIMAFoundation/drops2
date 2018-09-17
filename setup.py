4b3446aca768e77c41104139a726efe39fbb7549
Mirko D'Andrea <mirko.dandrea@gmail.com>
2018-09-17 13:43:29 +0200

    added setup.py and LICENSE for pypi package

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drops2",
+    version="0.0.2",
+    author="Mirko D'Andrea",
+    author_email="mirko.dandrea@cimafoundation.org",
+    description="dds data access api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CIMAFoundation/drops2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'decorator',
+        'geopandas',
+        'numpy',
+        'pandas',
+        'requests',
+        'scipy',
+        'xarray',
+      ],
+)
