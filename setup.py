8d80e670622febf9b191901a1c9bc0e4daf31855
Mirko D'Andrea <mirko.dandrea@gmail.com>
2020-06-26 11:57:36 +0200

    version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="drops2",
-    version="0.4.0",
+    version="0.5.0",
     author="Mirko D'Andrea",
     author_email="mirko.dandrea@cimafoundation.org",
     description="dds data access api",
