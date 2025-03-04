8e8661018d980e643b2982217cc98dd7332ceb87
pynthu72 <antonello@acrotec.it>
2025-03-04 14:01:36 +0100

    fix: change data retrieval to return raw data instead of pandas DataFrame

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from drops2 import sensors
 from drops2.utils import DropsCredentials
 
+if __name__ == '__main__':
 
-data = sensors.get_sensor_data_aggr(
-    'TERMOMETRO', 
-    ["210331261_2", "50000359_2", "210329479_2", "50000377_2", "50000428_2"], 
-    '202410010000', 
-    '202410050000', 
-    aggr_time=3600, 
-    aggr_func='AVERAGE', 
-    auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")),
-    date_as_string=True, 
-    as_pandas=True
-)
+    data = sensors.get_sensor_data('TERMOMETRO', 
+                                   ["210331261_2", "50000359_2", "210329479_2", "50000377_2", "50000428_2"], 
+                                   '202410010000', 
+                                   '202410050000', 
+                                   aggr_time=3600, 
+                                   aggr_func='MAXIMUM', 
+                                   auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")),
+                                   date_as_string=True, 
+                                   as_pandas=False)
 
-data
+    print(data)
