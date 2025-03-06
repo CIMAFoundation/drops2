68604b08f77c3692aee0df05b3615108be048b17
pynthu72 <antonello@acrotec.it>
2025-03-06 14:38:42 +0100

    refactor: update main.py to improve sensor data retrieval for TERMOMETRO

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+import json
 from drops2 import sensors, coverages
 from drops2.utils import DropsCredentials
-s = sensors.get_sensor_list('TERMOMETRO')
-s.as_serializable()
 
-sensors.get_aggregation_functions('TERMOMETRO', auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")))
+s = sensors.get_sensor_list('TERMOMETRO', auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")))
+json_list = s.as_serializable()
+print(json_list[0]['station'])
 
-data = sensors.get_sensor_data_aggr(
-    'TERMOMETRO', 
-    s.list, 
-    '202410010000', 
-    '202410050000', 
-    aggr_time=3600, 
-    aggr_func='AVERAGE', 
-    auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")),
-    date_as_string=True, 
-    as_pandas=True
-)
+# sensors.get_aggregation_functions('TERMOMETRO', auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")))
+
+# data = sensors.get_sensor_data_aggr(
+#     'TERMOMETRO', 
+#     s.list, 
+#     '202410010000', 
+#     '202410050000', 
+#     aggr_time=3600, 
+#     aggr_func='AVERAGE', 
+#     auth=DropsCredentials("https://dds-test.cimafoundation.org/dds/rest", ("admin", "geoDDS2013")),
+#     date_as_string=True, 
+#     as_pandas=True
+# )
 
 
 
